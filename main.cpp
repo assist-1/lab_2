69b9571891e61527fd2251f8a3a479de6f9bb7a2
Aleksandra Meshkova <poonghaneun@gmail.com>
2022-11-05 15:09:16 +0300

    Add command line parse

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
+#include <cstring>
 #include <iostream>
 
+void usage() {
+    std::cerr << "Usage: lab_2 [--forward] [--file filename] [--reverse]\n"
+              << "  --forward - read mathematical expression from stdin\n"
+              << "  --file filename - read mathematical expression from file\n"
+              << "  --reverse - use reverse Polish notation" << std::endl;
+}
+
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        std::cerr << "Nothing was read\n";
-        return EXIT_FAILURE;
-    } else {
-        std::cout << "You entered " << argc - 1 << " arguments\n";
-        for (std::size_t i = 1; i < argc; ++i) {
-            std::cout << argv[i] << std::endl;
+    bool forward = false;
+    bool file = false;
+    char *filename = nullptr;
+    bool reverse = false;
+    for (std::size_t i = 1; i < argc; ++i) {
+        if (std::strcmp(argv[i], "--forward") == 0) {
+            forward = true;
+        } else if (std::strcmp(argv[i], "--file") == 0) {
+            file = true;
+            filename = argv[i + 1];
+            ++i;
+        } else if (std::strcmp(argv[i], "--reverse") == 0) {
+            reverse = true;
+        } else {
+            std::cerr << "Unexpected argument: " << argv[i] << "\n";
+            usage();
+            return EXIT_FAILURE;
         }
+    }
+    if (file == forward) {
+        std::cerr << "You have to choose --forward or --file\n";
+        usage();
+        return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
