f7cad50c5ae629171e3cbf251e0fb339241332a5
Aleksandra Meshkova <poonghaneun@gmail.com>
2022-11-05 15:52:22 +0300

    Update calculator structure

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,17 @@
 #include "calculator.h"
 
-void calculate(std::istream &input_stream, bool reverse) {
+void reverse_calculate(std::istream &input_stream) {
 
+}
+
+void usual_calculate(std::istream &input_stream) {
+
+}
+
+void calculate(std::istream &input_stream, bool reverse) {
+    if (reverse) {
+        reverse_calculate(input_stream);
+    } else {
+        usual_calculate(input_stream);
+    }
 }
