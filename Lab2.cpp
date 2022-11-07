5902598f8efedf0f1e4d2ab3c1074aa83cb4e4b3
Manchdog <116804617+Manchdog@users.noreply.github.com>
2022-11-07 19:27:56 +0300

    Update Lab2.cpp

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,49 +1,40 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
+#include <cstring>
 #include "Lab2.h"
 int flag_file;
-int main()
+int main(int argc, char* argv[])
 {
     char s[40];
     float answer = -1000000000;
-    std::cout << "ЕСЛИ ХОТИТЕ СЧИТАТЬ ИЗ ФАЙЛА, ВВЕДИТЕ \"--file\", В ПРОТИВНОМ СЛУЧАЕ \"no\"" << std::endl;
-    std::cin >> s;
-    if (s[0] == 'n' && s[1] == 'o' && s[2] == '\0') {
-        flag_file = 0;
-        std::cout << "ВВЕДИТЕ ФЛАГ" << std::endl;
-        std::cin >> s;
-        if (s[0] == '-' && s[1] == '-' && s[2] == 'r' && s[3] == 'e' && s[4] == 'v' && s[5] == 'e' && s[6] == 'r' && s[7] == 's' && s[8] == 'e' && s[9] == '\0') {
-            std::cout << "ВВЕДИТЕ ВЫРАЖЕНИЕ С ПРОБЕЛАМИ В ОБРАТНОЙ ПОЛЬСКОЙ НОТАЦИИ И ПОСТАВЬТЕ В КОНЦЕ ВЫРАЖЕНИЯ \'=\'" << std::endl;
-            answer = OPN();
+    if (argc == 3) {
+        if (!strcmp(argv[1], "--file") && !strcmp(argv[2], "--forward") || !strcmp(argv[1], "--forward") && !strcmp(argv[2], "--file")) {
+            flag_file = 1;
+            answer = pr();
         }
-        else if (s[0] == '-' && s[1] == '-' && s[2] == 'f' && s[3] == 'o' && s[4] == 'r' && s[5] == 'w' && s[6] == 'a' && s[7] == 'r' && s[8] == 'd' && s[9] == '\0') {
+        else if (!strcmp(argv[1], "--file") && !strcmp(argv[2], "--reverse") || !strcmp(argv[1], "--reverse") && !strcmp(argv[2], "--file")) {
+            flag_file = 1;
+            answer = OPN();;
+        }
+        else {
+            std::cout << "ERROR! Flag is not correct" << std::endl;
+        }
+    }
+    else if (argc == 2) {
+        if (!strcmp(argv[1], "--forward")) {
             std::cout << "ВВЕДИТЕ ВЫРАЖЕНИЕ С ПРОБЕЛАМИ И ПОСТАВЬТЕ В КОНЦЕ ВЫРАЖЕНИЯ \'=\'" << std::endl;
             answer = pr();
         }
-        else {
-            std::cout << "ERROR! Flag is not correct" << std::endl;
-        }
-    }
-    else if (s[0] == '-' && s[1] == '-' && s[2] == 'f' && s[3] == 'i' && s[4] == 'l' && s[5] == 'e' && s[6] == '\0') {
-        flag_file = 1;
-        std::ifstream file;
-        file.open("flag.txt");
-        file >> s;
-        if (s[0] == '-' && s[1] == '-' && s[2] == 'r' && s[3] == 'e' && s[4] == 'v' && s[5] == 'e' && s[6] == 'r' && s[7] == 's' && s[8] == 'e'/* && s[9] == '\0'*/) {
+        else if (!strcmp(argv[1], "--reverse")) {
+            std::cout << "ВВЕДИТЕ ВЫРАЖЕНИЕ С ПРОБЕЛАМИ В ОБРАТНОЙ ПОЛЬСКОЙ НОТАЦИИ И ПОСТАВЬТЕ В КОНЦЕ ВЫРАЖЕНИЯ \'=\'" << std::endl;
             answer = OPN();
         }
-        else if (s[0] == '-' && s[1] == '-' && s[2] == 'f' && s[3] == 'o' && s[4] == 'r' && s[5] == 'w' && s[6] == 'a' && s[7] == 'r' && s[8] == 'd'/* && s[9] == '\0'*/) {
-            answer = pr();
-        }
         else {
             std::cout << "ERROR! Flag is not correct" << std::endl;
         }
-        file.close();
-
     }
     else {
-        std::cout << "ERROR! Flag is not correct" << std::endl;
-
+        std::cout << "ERROR! Too many flags or not enough" << std::endl;
     }
     if (answer > -1000000000) { std::cout << answer << std::endl; }
     return 0;
