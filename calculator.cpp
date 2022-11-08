c5ff978e5d400c07f973196759f3748d829b87ec
Aleksandra Meshkova <poonghaneun@gmail.com>
2022-11-08 20:09:15 +0300

    Add tokens

--- calculator.cpp
+++ calculator.cpp
@@ -1,17 +1,20 @@
 #include "calculator.h"
+#include "token.h"
 
-void reverse_calculate(std::istream &input_stream) {
+void reverse_calculate(Token* tokens) {
 
 }
 
-void usual_calculate(std::istream &input_stream) {
+void usual_calculate(Token* tokens) {
 
 }
 
 void calculate(std::istream &input_stream, bool reverse) {
+    Token tokens[100];
+    // TODO заполнить токены
     if (reverse) {
-        reverse_calculate(input_stream);
+        reverse_calculate(tokens);
     } else {
-        usual_calculate(input_stream);
+        usual_calculate(tokens);
     }
 }
---
+++ token.cpp
@@ -0,0 +1,5 @@
+#include "token.h"
+
+Token::Token(std::istream& input_stream) {
+    // TODO заполнить токен
+}
