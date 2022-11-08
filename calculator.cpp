6b6d2c641d569ac27569963446cfd3e669fa4015
Aleksandra Meshkova <poonghaneun@gmail.com>
2022-11-08 23:21:02 +0300

    Add reverse calculate

--- calculator.cpp
+++ calculator.cpp
@@ -4,6 +4,7 @@
 struct ListNode {
     Token token;
     ListNode *next = nullptr;
+    ListNode *prev = nullptr;
 };
 
 struct TreeNode {
@@ -29,8 +30,60 @@ int evaluate(TreeNode *root) {
     }
 }
 
+ListNode *pop(ListNode *stack_top) {
+    if (!stack_top->prev) {
+        delete stack_top;
+        return nullptr;
+    }
+    stack_top = stack_top->prev;
+    delete stack_top->next;
+    stack_top->next = nullptr;
+    return stack_top;
+}
+
+ListNode *push(ListNode *stack_top, Token token) {
+    if (!stack_top) {
+        stack_top = new ListNode();
+    } else {
+        stack_top->next = new ListNode();
+        stack_top->next->prev = stack_top;
+        stack_top = stack_top->next;
+    }
+    stack_top->token = token;
+    return stack_top;
+}
+
 int reverse_calculate(ListNode *head) {
-    return 0;
+    ListNode *stack_top = nullptr;
+    for (ListNode *p = head; p != nullptr; p = p->next) {
+        if (p->token.type == '0') {
+            stack_top = push(stack_top, p->token);
+        } else {
+            Token rhs = stack_top->token;
+            stack_top = pop(stack_top);
+
+            Token lhs = stack_top->token;
+            stack_top = pop(stack_top);
+
+            Token res;
+            switch (p->token.type) {
+                case '+':
+                    res.value = lhs.value + rhs.value;
+                    break;
+                case '-':
+                    res.value = lhs.value - rhs.value;
+                    break;
+                case '*':
+                    res.value = lhs.value * rhs.value;
+                    break;
+                case '/':
+                    res.value = lhs.value / rhs.value;
+                    break;
+            }
+            stack_top = push(stack_top, res);
+        }
+    }
+    return stack_top->token.value;
 }
 
 TreeNode *usual_parse(ListNode *head, ListNode *tail) {
@@ -81,7 +134,7 @@ int calculate(std::istream &input_stream, bool reverse) {
             p = head;
         } else {
             p->next = new ListNode();
-            p = p->next;
+            p = p->next; // односвязный список (prev не заполняем)
         }
         p->token = Token(input_stream);
     }
